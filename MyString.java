cbdd5cafa50f6bb75a5b4a2994abefa7a4b48394
Isabella Rocha <irocha00@stuy.edu>
2018-11-22 12:51:49 -0500

    Added exceptions

--- MyString.java
+++ MyString.java
@@ -3,20 +3,29 @@ public class MyString implements CharSequence {
   public MyString (CharSequence s) {
     for(int idx = 0; idx < s.length(); idx ++) {
       data[idx] = charAt(idx);
+      // Goes and loops through the CharSequence and adds each element to the array
     }
   }
   public char charAt(int idx) {
+    if (idx < 0 || idx >= length()) {
+      throw new IndexOutOfBoundsException();
+    }
     return data[idx];
   }
   public int length() {
     return data.length;
   }
   public CharSequence subSequence(int start, int end) {
+    if (start < 0 || end < 0 || start > end || end > length()) {
+      throw new IndexOutOfBoundsException();
+    }
+    // Creates a string from start to end to input into MyString since MyString is-a CharSequence
     String output = "";
     for (int idx = start; idx < end; idx ++) {
       output += data[idx];
     }
     CharSequence out = new MyString(output);
+    // Converting string to CharSequence
     return out;
   }
   public String toString() {
